18d6449ca6ed4838d14d79235a7aaa8be9eb7cd2
ubada <ubadafatta@gmail.com>
2024-06-02 20:31:41 +0200

    trying to fix pages

--- src/js/resources.js
+++ src/js/resources.js
@@ -1,14 +1,14 @@
 import { ImageSource, Loader } from 'excalibur';
 
 const Resources = {
-    Bus: new ImageSource('docs/bus.png'),
-    Oil: new ImageSource('docs/oil.png'),
-    Smashed: new ImageSource('docs/smashed.png'),
-    GameOverBG: new ImageSource('docs/gameoverBG.png'),
-    Mofi: new ImageSource('docs/mofi.png'),
-    MofiSpriteSheet: new ImageSource('docs/mofispritesheet.png'),
-    CarSpriteSheet: new ImageSource('docs/carspritesheet.png'),
-    Background: new ImageSource('docs/background.png')
+    Bus: new ImageSource('docs/images/bus.png'),
+    Oil: new ImageSource('docs/images/oil.png'),
+    Smashed: new ImageSource('docs/images/smashed.png'),
+    GameOverBG: new ImageSource('docs/images/gameoverBG.png'),
+    Mofi: new ImageSource('docs/images/mofi.png'),
+    MofiSpriteSheet: new ImageSource('docs/images/mofispritesheet.png'),
+    CarSpriteSheet: new ImageSource('docs/images/carspritesheet.png'),
+    Background: new ImageSource('docs/images/background.png')
 };
 
 const ResourceLoader = new Loader(Object.values(Resources));
