219274ced5493ff48bc7a01992503bde8405b4bb
ubada <ubadafatta@gmail.com>
2024-06-02 20:46:39 +0200

    trying to fix pages

--- src/js/resources.js
+++ src/js/resources.js
@@ -1,16 +1,17 @@
 import { ImageSource, Loader } from 'excalibur';
 
-const Resources = {
-    Bus: new ImageSource('images/bus.png'),
-    Oil: new ImageSource('images/oil.png'),
-    Smashed: new ImageSource('images/smashed.png'),
-    GameOverBG: new ImageSource('images/gameoverBG.png'),
-    Mofi: new ImageSource('images/mofi.png'),
-    MofiSpriteSheet: new ImageSource('images/mofispritesheet.png'),
-    CarSpriteSheet: new ImageSource('images/carspritesheet.png'),
-    Background: new ImageSource('images/background.png')
-};
+const basePath = '/Mofi-s'; // Update 'repository' to your repo name if needed
 
+const Resources = {
+    Bus: new ImageSource(`${basePath}/docs/images/bus.png`),
+    Oil: new ImageSource(`${basePath}/docs/images/oil.png`),
+    Smashed: new ImageSource(`${basePath}/docs/images/smashed.png`),
+    GameOverBG: new ImageSource(`${basePath}/docs/images/gameoverBG.png`),
+    Mofi: new ImageSource(`${basePath}/docs/images/mofi.png`),
+    MofiSpriteSheet: new ImageSource(`${basePath}/docs/images/mofispritesheet.png`),
+    CarSpriteSheet: new ImageSource(`${basePath}/docs/images/carspritesheet.png`),
+    Background: new ImageSource(`${basePath}/docs/images/background.png`)
+};
 const ResourceLoader = new Loader(Object.values(Resources));
 
 export { Resources, ResourceLoader };
